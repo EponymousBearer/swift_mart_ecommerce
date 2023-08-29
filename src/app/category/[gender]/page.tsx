3d17a912b2b304bd918549b60103f6fa386462d8
EponymousBearer <adnasir607@gmail.com>
2023-08-29 18:54:40 +0500

    bugs fixed

--- src/app/category/[gender]/page.tsx
+++ src/app/category/[gender]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FetchData from "../../../../sanity/FetchData";
 import ProductCart from "@/components/ProductCart";
+import Link from "next/link";
 
 interface IProductImage {
   _type: string;
@@ -29,7 +30,9 @@ interface IProduct {
 export default async function CategoryPage({ params }: { params: any }) {
   const data = await FetchData();
 
-  const filteredData = data.filter((item: IProduct) => item.gender === params.gender);
+  const filteredData = data.filter(
+    (item: IProduct) => item.gender === params.gender
+  );
 
   if (filteredData.length === 0) {
     return <div className="text-center mt-20 font-bold">No Products found</div>;
@@ -39,7 +42,9 @@ export default async function CategoryPage({ params }: { params: any }) {
     <section>
       <div className="grid grid-cols-[repeat(1,auto)] lg:grid-cols-[repeat(3,auto)] xl:grid-cols-[repeat(4,auto)] gap-14 mt-10 justify-center">
         {filteredData.map((product: IProduct, index: number) => (
-          <ProductCart item={product} key={product._id} />
+          <Link href={`/product/${product.slug.current}`} key={index}>
+            <ProductCart item={product} key={product._id} />
+          </Link>
         ))}
       </div>
     </section>
